f9f431da007b573275760c7b4e3cdebcd359b2c6
Tobias Waslowski <tobiaswaslowski@gmail.com>
2021-09-12 17:07:18 +0200

    Refactorings

--- src/main/scala/de/nordakademie/reaktiv/demo/Greeter.scala
+++ src/main/scala/de/nordakademie/reaktiv/demo/Greeter.scala
@@ -5,8 +5,8 @@ import akka.actor.Actor
 class Greeter extends Actor {
 
   def receive: Receive = {
-    case m : Message => m.recipient ! m.content
-    case s : String => println(s"Received message ${s}")
+    case Message(recipient, content) => recipient ! content
+    case text: String => println(s"Received message ${text}")
     case _ => println("I'm afraid I can't do anything with this.")
   }
 
